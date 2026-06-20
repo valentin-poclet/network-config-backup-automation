a700044d0a970e6cf05480cc6fc558ddcb047d80
valentin-poclet <valentin.poclet@edu.esiee.fr>
2026-06-20 15:00:55 +0200

    Update backup_configs.py

--- backup_configs.py
+++ backup_configs.py
@@ -1,1 +1,14 @@
-print("Projet Network Backup lancé")
+config = """
+hostname R1
+
+interface GigabitEthernet0/0
+ ip address 192.168.1.1 255.255.255.0
+
+router ospf 1
+ network 192.168.1.0 0.0.0.255 area 0
+"""
+
+with open("backups/R1_config.txt", "w") as fichier:
+    fichier.write(config)
+
+print("Sauvegarde créée")
